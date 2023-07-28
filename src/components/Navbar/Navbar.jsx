117c972833d1e73535fd15566286c7527a3d76fa
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2023-07-28 23:48:56 +0100

    navbar lvl2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import "./Navbar.scss"
+import {motion} from "framer-motion"
+import {HiX} from "react-icons/hi"
 
+import "./Navbar.scss"
 
 
 const Navbar = () => {
 
         const [scrollPos, setScrollPos] = useState(0);
-
+        const [screen, setScreen] = useState(0)
+        const [toggle, setToggle] = useState(false)
 
 
         useEffect(() => {
@@ -14,19 +17,71 @@ const Navbar = () => {
                         setScrollPos(window.pageYOffset);
                 }
 
+                const handleResize = () => {
+                        setScreen(window.innerWidth);
+                }
+                
                 //initialize
+                handleResize();
                 handleScroll()
-                console.log("scrollPos", scrollPos)
-
+                
                 window.addEventListener('scroll', handleScroll);
+                window.addEventListener("resize", handleResize);
                 
                 return () => {
                         window.removeEventListener('scroll', handleScroll);
+                        window.removeEventListener("resize", handleResize);
                 };
+                        // eslint-disable-next-line react-hooks/exhaustive-deps
         }, []);
+        
 
   return (
-    <div>Navbar</div>
+    <nav className={`${"navbar_container"}  ${scrollPos >= 5  && "blur_navbar"   }  `}>
+        <div className='app__navbar-logo'>
+                <a href="#home">
+                        <img src={"/logo.png"}  alt="logo" />
+                </a>
+        </div>
+
+        <div className='app__navbar-menu'>
+        {
+                screen<500 ?
+                <>
+                        <span id="menu-btn" class={`block  hamburger ${toggle&&"open"}`}  onClick={()=>setToggle(val=>!val)}>
+                                <span class="hamburger-1"></span>
+                                <span class="hamburger-2"></span>
+                                <span class="hamburger-3"></span>
+                        </span>
+
+                        {
+                        toggle && (
+                        <motion.div  whileInView={{opacity:[0,1]}}  transition={{duration:.5, type:"easeOut"}} >
+                                <HiX onClick={()=>setToggle(false)}/>
+                                
+                                <ul>
+                                {
+                                ["home","about","work","skills","testimonials","contact" ].map((item)=>(
+                                        <li  className='' key={item}>
+                                        <a href={`#${item}`} onClick={()=>setToggle(false)}> {item} </a>
+                                        </li>
+                                ))
+                                }
+                                </ul>
+                        </motion.div>
+                        )
+                        }
+                        
+                </>
+                :
+                <>
+                        <button>Our services</button>
+                        <button>Join our newsletter</button>
+                </>
+        }
+        </div>
+
+    </nav>
   )
 }
 
