11f279cf1ad90573108b5d63b21b5b9a4cd7343c
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2023-08-14 13:26:34 +0100

    fix verba styling

--- src/sections/Services/Services.jsx
+++ src/sections/Services/Services.jsx
@@ -10,33 +10,33 @@ import Veba from '../../components/Veba'
 
 
 
-const services = [  {
+const services = [{
         name: ["GPT-3 | GPT 4", "Development"],
         services: [
-          "Language synthesis",
-          "Knowledge response",
-          "Chatbot development",
-          "Model tuning",
+                "Language synthesis",
+                "Knowledge response",
+                "Chatbot development",
+                "Model tuning",
         ],
-      },
-      {
+},
+{
         name: ["AI", "Consulting"],
         services: [
-          "Feasibility assessments",
-          "Use case identification",
-          "Performance evaluation",
-          "Strategy development",
+                "Feasibility assessments",
+                "Use case identification",
+                "Performance evaluation",
+                "Strategy development",
         ],
-      },
-      {
+},
+{
         name: ["Natural Language", "Processing"],
         services: [
-          "Document categorization",
-          "Data mining",
-          "Text summarization",
-          "Entity identification",
+                "Document categorization",
+                "Data mining",
+                "Text summarization",
+                "Entity identification",
         ],
-      },
+},
 ];
 
 
@@ -47,59 +47,60 @@ const Services = () => {
                 const handleResize = () => {
                         setScreen(window.innerWidth);
                 }
-                
+
                 //initialize
                 handleResize();
-                
+
                 window.addEventListener("resize", handleResize);
-                
+
                 return () => {
                         window.removeEventListener("resize", handleResize);
                 };
-                        // eslint-disable-next-line react-hooks/exhaustive-deps
+                // eslint-disable-next-line react-hooks/exhaustive-deps
         }, []);
-        
-  return (
-        <>
-                <h2 className='subheader'>OUR SERVICES</h2>
 
-                <Veba/>
+        return (
+                <>
+                        <h2 className='subheader'>OUR SERVICES</h2>
+                        <div className="veba_canva">
+                                <Veba />
+                        </div>
 
-                {
-                        screen>770 ?
-                        <section className='serve-holder'>
                         {
-                                services.map((service, index) => (
-                                        <div className="serve-card" key={index}>
-                                                <span className="deco" />
-                                                <h4 className="title">{service.name[0]}<br/>{service.name[1]}</h4>
-                                                <div className='caption'>{service.services.map((serve, i)=>(<p key={i}>{serve}</p>) )}</div>
-                                        </div>
-                                ))
+                                screen > 770 ?
+                                        <section className='serve-holder'>
+                                                {
+                                                        services.map((service, index) => (
+                                                                <div className="serve-card" key={index}>
+                                                                        <span className="deco" />
+                                                                        <h4 className="title">{service.name[0]}<br />{service.name[1]}</h4>
+                                                                        <div className='caption'>{service.services.map((serve, i) => (<p key={i}>{serve}</p>))}</div>
+                                                                </div>
+                                                        ))
+                                                }
+                                        </section>
+                                        :
+                                        <Carousel className="" autoPlay={true} infiniteLoop={true} showStatus={false} >
+                                                {
+                                                        services.map((service, index) => {
+                                                                return (
+                                                                        <div className="serve-card" key={index}>
+                                                                                <span className="deco" />
+                                                                                <h4 >{service.name}</h4>
+                                                                                <div className='caption'>{service.services.map((serve, i) => (<p key={i}>{serve}</p>))}</div>
+                                                                        </div>
+                                                                );
+                                                        })
+                                                }
+                                        </Carousel>
                         }
-                        </section>
-                        :
-                        <Carousel className="" autoPlay={true}  infiniteLoop={true} showStatus={false} >
-                        {
-                                services.map((service, index) => {
-                                        return (
-                                        <div className="serve-card" key={index}>
-                                                <span className="deco" />
-                                                <h4 >{service.name}</h4>
-                                                <div className='caption'>{service.services.map((serve, i)=>(<p key={i}>{serve}</p>) )}</div>
-                                        </div>
-                                        );
-                                })
-                        }
-                        </Carousel>
-                }
 
 
-                
-                
 
-        </>
-  )     
+
+
+                </>
+        )
 }
 
-export default AppWrap( MotionWrap(Services,"app__services" ) , "services", "app__whitebg ") 
+export default AppWrap(MotionWrap(Services, "app__services"), "services", "app__whitebg ") 
